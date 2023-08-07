7ef2359e848b5939ebe39cef5cd45cd95ad05bfc
Jessica Carrillo <jessymamo@gmail.com>
2023-08-07 16:19:34 -0400

    Completed lab

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
+
 /* 
-  Write the Big O time complexity of your function here
+  I don't know
 */
 
 /* 
-  Add your pseudocode here
-*/
+  get first element of the array and add it to the rest of the rest of the elements
+  get the second element and add it to the rest (except the first)
+  do this until iterate the last minus -1 element
+
+  3 + 8, 3+12, 3+4, 3+ 11, 3 + 7, 
+  8 + 12, 8+4, 8+11, 8+7
+  12 + 4, 12+11, 12+7
+  4+11,4+7
+  11 + 7
+
+if the addition of a pair === the target number return true else return false
+  
 
 /*
-  Add written explanation of your solution here
+  Write a function that returns true if any pair number add up to a target number. Function takes two arguments: array containing numbers and the target number
 */
 
 // You can run `node index.js` to view these console logs
